b9643b7f542b3956b57d51a773797e344a1b39ac
rohit <rohit@DESKTOP-FNKE01M>
2019-10-08 21:02:50 -0700

    files for assignment 2

---
+++ cse360Assign2/src/cse360Assign2/AddingMachine.java
@@ -0,0 +1,23 @@
+package cse360Assign2;
+
+public class AddingMachine {
+	
+	private int total;
+	// not needed - included for clarity
+	public AddingMachine () 
+	{
+		total = 0; 
+	}
+	public int getTotal () 
+	{
+		return 0;
+	}
+	public void add (int value) {}
+	public void subtract (int value) {}
+	public String toString () 
+	{
+		return "";
+	}
+	public void clear() {}
+
+}
